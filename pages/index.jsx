45e57a28ec38b49d93f7a4b539a58ead40208582
“JavadRafieii” <j.rafiei999@gmail.com>
2024-05-13 15:26:29 +0330

    The home page slider is finished.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-import Container from "@/theme/container";
+import MainSlider from "@/components/mainSlider/mainSlider";
 
 export default function HomePage() {
   return (
-    <Container>
-      <h1 className="text-golden">Home Page</h1>
-    </Container>
+    <main>
+      <MainSlider />
+    </main>
   );
 };
